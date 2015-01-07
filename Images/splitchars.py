c1e807f36330b4f0cb34b49e453d57ae63887ffc
Per Olofsson <MagerValp@gmail.com>
2015-01-07 10:55:42 +0100

    Created project.

---
+++ Images/splitchars.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+import sys
+import argparse
+import png
+
+
+def print8(*args):
+    print " ".join(unicode(x).encode(u"utf-8") for x in args)
+
+
+def main(argv):
+    p = argparse.ArgumentParser()
+    p.add_argument(u"-v", u"--verbose", action=u"store_true",
+                   help=u"Verbose output.")
+    p.add_argument(u"source")
+    p.add_argument(u"outprefix")
+    args = p.parse_args([x.decode(u"utf-8") for x in argv[1:]])
+    
+    r = png.Reader(filename=args.source)
+    width, height, pixels, metadata = r.asRGBA8()
+    lines = list(pixels)
+    
+    w = 8
+    h = 8
+    for i in xrange(0, 256):
+        y = int(i / 32)
+        x = i % 32
+        char = list()
+        for dy in xrange(0, 8):
+            char.append(lines[y * 8 + dy][x * 4 * 8:(x + 1) * 4 * 8])
+        w = png.Writer(width=8, height=8, alpha=True)
+        with open(u"%s%02x.png" % (args.outprefix, i), u"w") as f:
+            w.write(f, char)
+    
+    return 0
+    
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
+    
--- c02emu/GameScene.swift
+++ c02emu/GameScene.swift
@@ -9,12 +9,37 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var screenCharNodes: [SKSpriteNode!]!
+    var screenCharNodesSetup = false
+    var counter = 0
+    var charTextures: [SKTexture!]!
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        myLabel.text = "Hello, World!"
+        myLabel.fontSize = 65
+        myLabel.fontColor = SKColor.greenColor()
+        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        
+        screenCharNodes = []
+        for y in 0..<50 {
+            for x in 0..<80 {
+                let i = (y % 8) * 32 + x % 32
+                let sprite = SKSpriteNode(imageNamed:String(format:"charset_%02x", i))
+                sprite.anchorPoint = CGPointZero
+                sprite.position = self.convertPointFromView(CGPoint(x: x*8, y: 400 - y*8 - 8))
+                self.addChild(sprite)
+                screenCharNodes.append(sprite)
+            }
+        }
+        charTextures = []
+        for i in 0..<256 {
+            charTextures.append(SKTexture(imageNamed: String(format:"charset_%02x", i)))
+        }
+        
+        self.screenCharNodesSetup = true
         
         self.addChild(myLabel)
     }
@@ -24,8 +49,10 @@ class GameScene: SKScene {
         
         let location = theEvent.locationInNode(self)
         
+        NSLog("location: \(location)")
+        
         let sprite = SKSpriteNode(imageNamed:"Spaceship")
-        sprite.position = location;
+        sprite.position = location
         sprite.setScale(0.5)
         
         let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
@@ -36,5 +63,12 @@ class GameScene: SKScene {
     
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        
+        if self.screenCharNodesSetup {
+            for i in 0..<80 * 50 {
+                screenCharNodes[i].texture = self.charTextures[(i + self.counter) & 0xff]
+            }
+            self.counter++
+        }
     }
 }
